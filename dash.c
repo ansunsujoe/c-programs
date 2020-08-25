0f44815c07d4af898a4bfb3425b1a820ae53678e
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2020-08-24 22:06:53 -0500

    Dummy functions for simple execution and redirect

--- dash.c
+++ dash.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 /**
  * Dash program
  */
 
 int main() {
-    char input[30];
-    char check[30];
-
-    int sysres = 0;
+    // Variables for reading the command
+    char *input;
+    size_t input_size = 100;
+    char *arguments[10];
 
     while (1) {
         printf("dash> ");
-        scanf("%s", input);
+
+        // Get the input using getline
+        getline(&input, &input_size, stdin);
+
+        // Convert to array of arguments
+        *arguments = convertToArray(input);
 
         // Check if input equals exit
         if (strcmp(input, "exit") == 0) {
             printf("Exiting now...\n");
-            break;  // Break out of loop
+            exit(0);
         }
 
         // Check if input equals ls
@@ -31,4 +37,33 @@ int main() {
             printf("Command %s is not recognized\n", input);
         }
     }
+}
+
+/**
+ * Convert the string we received into an array of arguments
+ */
+char* convertToArray(char *input) {
+    char *arguments[10];
+    char *name = strtok(input, " ");
+}
+
+/**
+ * Executes a command using execv and fork
+ */
+void execute_command() {
+
+}
+
+/**
+ * Identifies if an array of characters contains redirection
+ */
+bool isRedirecting() {
+    return true;
+}
+
+/**
+ * Perform the redirect to a file
+ */
+void redirect(char *filename) {
+
 }

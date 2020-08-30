30f2f796575f6908789b9139e004bf2a1c617e30
s.bose <s.bose@ignited2k.com>
2020-08-30 16:35:54 -0500

    Found places to split function execution for parallel commands

--- dash.c
+++ dash.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+
 // Forward declaration of functions
 void convertToArray(char input[], char *array);
 
@@ -11,8 +12,6 @@ void execute_command_substring(char *args[], int startIndex, int endIndex);
 
 bool isRedirecting(char *args[], int startIndex, int endIndex);
 void redirect(char *args[]);
-
-int* findParallelSplits(char *args[])
 
 
 /**
@@ -23,7 +22,7 @@ int main() {
     // Variables for reading the command
     char *input;
     size_t input_size = 100;
-    char *arguments[10];
+    char *arguments[20];
 
     while (1) {
         printf("dash> ");
@@ -41,34 +40,33 @@ int main() {
         }
         
         // Find the array length
-        int argsLength = strlen(*arguments) + 1;
+        int argsLength = strlen(*arguments);
+        
+        // Find where to split parallel commands (&)
+        int parallelSplits[10];
+		int splitCount = 0;
+	
+		// Iterate through arguments in search of &
+		for (int j = 0; j < argsLength; j++) {
+			if (strcmp(arguments[j], "*") == 0) {
+		
+				// Add the index of the split to the list
+				parallelSplits[splitCount] = j;
+				splitCount++;
+			}
+		}
 
-        // Execute the command
+        // Execute the command(s)
         execute_command_substring(arguments, 0, argsLength);
 
-        // Check if input equals exit
-        if (strcmp(input, "exit") == 0) {
-            printf("Exiting now...\n");
-            exit(0);
-        }
-
-        // Check if input equals ls
-        else if (strcmp(input, "ls") == 0) {
-            system("ls");
-        }
-
-        // If invalid, print that an invalid command was typed and contine
-        else {
-            printf("Command %s is not recognized\n", input);
-        }
     }
 }
 
 /**
- * Executes a command using execv and fork
+ * Executes a command using execv (the fork part will be done in the main method)
  */
 void execute_command(char *args[]) {
-    // Dummy function - needs to be filled in with exec and fork stuff
+    // Just print for now, but will be replaced by execv
     printf("Executing command %s...\n", args[0]);
 }
 
@@ -95,6 +93,8 @@ void execute_command_substring(char *args[], int startIndex, int endIndex) {
  * Identifies if an array of characters contains redirection
  */
 bool isRedirecting(char *args[], int startIndex, int endIndex) {
+	
+	// Finding the length of the array
     int arrayLen = strlen(*args) + 1;
     
     // Redirection symbol should be second to last in the array
@@ -113,12 +113,5 @@ bool isRedirecting(char *args[], int startIndex, int endIndex) {
  * Perform the redirect to a file
  */
 void redirect(char *args[]) {
-	
-}
-
-/**
- * Find where to split parallel commands (&)
- */
-int* findParallelSplits(char *args[]) {
 	
 }

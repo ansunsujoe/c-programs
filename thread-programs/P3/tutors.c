6fce147876ee8b782f5cd6dec1a25a852524736f
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2020-10-24 14:00:41 -0500

    Dequeue function

--- thread-programs/P3/tutors.c
+++ thread-programs/P3/tutors.c
@@ -2,37 +2,37 @@
 #include <pthread.h> 
 #include <semaphore.h> 
 #include <unistd.h>
+#include <stdlib.h>
 
 static int numStudents = 1;
 static int numTutors = 1;
 static int chairs = 10;
 static int helps = 1;
 
-typedef struct {
+struct person {
     int id;
     int priority;
-} person;
+};
 
-typedef struct {
-    person* student;
-    node* next;
-} node;
+struct node {
+    struct person* student;
+    struct node* next;
+};
 
-typedef struct {
-    node* head;
+struct LinkedList {
+    struct node* head;
     int length;
-} LinkedList;
-
-LinkedList *waitingRoom = {NULL, 0};
+};
 
 /**
  * Insert to the LinkedList of students in waiting room
  */
-void insert(LinkedList *waitingRoom, person *student) {
+void insert(struct LinkedList *waitingRoom, struct person *student) {
 
     // Iterator and new node
-    node *cur = waitingRoom->head;
-    node *newNode = {student, NULL};
+    struct node *cur = waitingRoom->head;
+    struct node *newNode = malloc(sizeof(struct node));
+    newNode->student = student;
 
     // Increase the length of the list
     waitingRoom->length++;
@@ -63,10 +63,33 @@ void insert(LinkedList *waitingRoom, person *student) {
     cur->next = newNode;
 }
 
+struct person* dequeue(struct LinkedList *waitingRoom) {
+    // Delete the head and free the memory given to the head node
+    struct node *deleteNode = waitingRoom->head;
+    waitingRoom->head = deleteNode->next;
+    
+    // Return the student structure for the tutor to see this
+    struct person *nextInLine = deleteNode->student;
+    free(deleteNode);
+    return nextInLine;
+}
+
+/**
+ * Print the waiting room for debugging purposes
+ */
+void printWaitingRoom(struct LinkedList *waitingRoom) {
+    printf("Waiting Room: \n");
+    struct node *cur = waitingRoom->head;
+    while (cur != NULL) {
+        printf("Student ID: %d, Priority %d\n", cur->student->id, cur->student->priority);
+        cur = cur->next;
+    }
+}
+
 /**
  * Queue in waiting room - Insert unless waiting room is full
  */
-int queueInWaitingRoom(person *student) {
+int queueInWaitingRoom(struct LinkedList *waitingRoom, struct person *student) {
 
     // The student will have to go back to programming and come back
     // another time
@@ -88,7 +111,26 @@ int studentsCurrentlyHelped;
 int tutoringSessionsCompleted;
 
 int main() {
-
+    struct LinkedList *waitingRoom = malloc(sizeof(struct LinkedList));
+    waitingRoom->length = 0;
+    struct person *student1 = malloc(sizeof(struct person));
+    struct person *student2 = malloc(sizeof(struct person));
+    struct person *student3 = malloc(sizeof(struct person));
+    struct person *student4 = malloc(sizeof(struct person));
+    student1->id = 521;
+    student2->id = 632;
+    student3->id = 400;
+    student4->id = 753;
+    student1->priority = 3;
+    student2->priority = 4;
+    student3->priority = 1;
+    student4->priority = 9;
+    insert(waitingRoom, student1);
+    insert(waitingRoom, student2);
+    insert(waitingRoom, student3);
+    insert(waitingRoom, student4);
+    dequeue(waitingRoom);
+    printWaitingRoom(waitingRoom);
     return 0;
 }
 

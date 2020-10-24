dd30c7f6d5754944bcbf2ad13257957b62d7985e
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2020-10-24 11:49:57 -0500

    Insert to waiting room linked list

--- thread-programs/P3/tutors.c
+++ thread-programs/P3/tutors.c
@@ -1,25 +1,83 @@
 #include <stdio.h> 
 #include <pthread.h> 
 #include <semaphore.h> 
-#include <unistd.h> 
+#include <unistd.h>
 
-struct {
+static int numStudents = 1;
+static int numTutors = 1;
+static int chairs = 10;
+static int helps = 1;
+
+typedef struct {
     int id;
     int priority;
 } person;
 
-struct {
-    struct person;
-    struct node* next;
+typedef struct {
+    person* student;
+    node* next;
 } node;
 
-struct {
-    struct node* head;
+typedef struct {
+    node* head;
     int length;
-} waitingRoom;
+} LinkedList;
 
-int queueInWaitingRoom() {
-    
+LinkedList *waitingRoom = {NULL, 0};
+
+/**
+ * Insert to the LinkedList of students in waiting room
+ */
+void insert(LinkedList *waitingRoom, person *student) {
+
+    // Iterator and new node
+    node *cur = waitingRoom->head;
+    node *newNode = {student, NULL};
+
+    // Increase the length of the list
+    waitingRoom->length++;
+
+    // If list is empty, then insert at head
+    if (cur == NULL) {
+        waitingRoom->head = newNode;
+        return;
+    }
+    // If the student has a greater priority than the head
+    if (student->priority > cur->student->priority) {
+        newNode->next = cur;
+        waitingRoom->head = newNode;
+        return;
+    }
+
+    // Iterate to find where to queue the student based on priority
+    while (cur->next != NULL) {
+        if (student->priority > cur->next->student->priority) {
+            newNode->next = cur->next;
+            cur->next = newNode;
+            return;
+        }
+        cur = cur->next;
+    }
+
+    // Lowest priority - place at end of list
+    cur->next = newNode;
+}
+
+/**
+ * Queue in waiting room - Insert unless waiting room is full
+ */
+int queueInWaitingRoom(person *student) {
+
+    // The student will have to go back to programming and come back
+    // another time
+    if (waitingRoom->length >= chairs) {
+        return -1;
+    }
+    // Priority queue insert
+    else {
+        insert(waitingRoom, student);
+    }
+    return 0;
 }
 
 // Global variables for output
